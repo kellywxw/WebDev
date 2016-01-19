d8626a89d982774b8bce2e4cf3cfe1136800d96e
Kelly Wang <kellywxw@ccs.neu.edu>
2016-01-19 15:52:18 -0800

    add static hello.js, hello.html, index.html

--- server.js
+++ server.js
@@ -2,7 +2,23 @@ var express = require('express');
 var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
-app.get('/hello', function(req, res){
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/hello', sayHello);
+
+app.get('/user', getAllUsers);
+
+function sayHello(req, res) {
     res.send('hello world');
-});
+}
+
+function getAllUsers(req, res) {
+    var users = [
+        {username: "alice", firstName: "Alice", lastName: "Wonderland"}
+    ];
+
+    res.json(users);
+}
+
 app.listen(port, ipaddress);
