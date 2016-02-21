b089b5b5140af6a34977ced1416b0444a75c6e00
Kelly Wang <kellywxw@ccs.neu.edu>
2016-02-21 14:04:55 -0800

    working on assignment2

--- public/assignment/views/header/header.controller.js
+++ public/assignment/views/header/header.controller.js
@@ -3,7 +3,22 @@
         .module("FormBuilderApp")
         .controller("HeaderController", HeaderController);
 
-    function HeaderController($scope, $location) {
+    function HeaderController($scope, $rootScope, $location) {
         $scope.$location = $location;
+
+        $scope.isAdmin = function isAdmin () {
+            var roles = $rootScope.user.roles;
+            for(var i = 0; i < roles.length; i++) {
+                if(roles[i] == "admin") {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        $scope.logout= function logout () {
+            $rootScope.user = null;
+            $location.url("/home");
+        }
     }
 })();
--- public/assignment/views/sidebar/sidebar.controller.js
+++ public/assignment/views/sidebar/sidebar.controller.js
@@ -3,7 +3,17 @@
         .module("FormBuilderApp")
         .controller("SidebarController", SidebarController);
 
-    function SidebarController($scope, $location) {
+    function SidebarController($scope, $rootScope, $location) {
         $scope.$location = $location;
+
+        $scope.isAdmin = function isAdmin () {
+            var roles = $rootScope.user.roles;
+            for(var i = 0; i < roles.length; i++) {
+                if(roles[i] == "admin") {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 })();
