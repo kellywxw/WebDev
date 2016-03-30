f2a21eb4e03fd7c086ec4480f540cd441a9de238
Kelly Wang <kellywxw@ccs.neu.edu>
2016-03-30 03:43:33 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,29 @@ var bodyParser = require('body-parser');
 var multer = require('multer');
 var express = require('express');
 var app = express();
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
 var mongoose = require('mongoose');
-var db = mongoose.connect('mongodb://localhost/FormBuilderDB');
+
+var connectionString = 'mongodb://localhost/FormBuilderDB';
+
+// use remote connection string
+// if running in remote server
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+var db = mongoose.connect(connectionString);
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
+app.use(multer());
+app.use(session({ secret: process.env.PASSPORT_SECRET , cookie: { maxAge: 60000 }, resave: true, saveUninitialized: true }));
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
