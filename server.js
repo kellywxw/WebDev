72580769fdadce59a2d6b617538284df20576c83
Kelly Wang <kellywxw@ccs.neu.edu>
2016-03-31 20:39:14 -0700

    fix openshift setup

--- server.js
+++ server.js
@@ -6,16 +6,8 @@ var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var mongoose = require('mongoose');
 
-
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port = process.env.OPENSHIFT_NODEJS_PORT ||process.env.PORT || 3000;
-
-app.listen(port, ipaddress);
-
-/*
 // create a default connection string
 var connectionString = 'mongodb://localhost/FormBuilderDB';
-
 
 // use remote connection string
 // if running in remote server
@@ -28,7 +20,6 @@ if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
 }
 
 var db = mongoose.connect(connectionString);
-*/
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
@@ -37,12 +28,13 @@ app.use(session({ secret: process.env.PASSPORT_SECRET}))
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
 
 require("./public/assignment/server/app.js")(app, mongoose, db);
 require("./public/project/server/app.js")(app, mongoose, db);
 
-
+app.listen(port, ipaddress);
 
 /*
  app.get('/hello', sayHello);
